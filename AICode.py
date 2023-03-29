4296263aa7e391b7f67251a2a3083184fd0ffbc0
“hewj” <hewj@mochasoft.com.cn>
2023-03-29 16:49:53 +0800

    志屁第1.1版： 1、专线底下要加上 直线的sheet 但是是空表 2、修改没有删除1和3列 3、设备列有值就单元格标黄（其余规则待定） 4、金额计算规则待定

--- AICode.py
+++ AICode.py
@@ -6,9 +6,11 @@
 import pandas as pd
 import os
 import datetime
+import numpy as np
 
 
 class KeNuo:
+    # 格式化日期，作为文档名称，并首次处理Excel源文件内容
     def __init__(self, file_path):
         # 获取当前日期
         today = datetime.date.today()
@@ -17,49 +19,74 @@ class KeNuo:
         # 读取Excel文件
         self.df = pd.read_excel(file_path, sheet_name=0)
         # 删除第1列和第3列
-        self.df.drop(self.df.columns[[0, 2]], axis=1)
+        self.df = self.df.drop(self.df.columns[[0, 2]], axis=1)
 
-    def money_modify(self, row):
-        if row['大件件数'] + row['冷链件数'] >= 50 and row['冷链件数'] < 10 and row['实付金额'] < 8000:
-            pay = (row['大件件数'] + row['冷链件数']) * 7
-            return f'送货费40元\n运费（{pay}）元'
-        elif row['大件件数'] + row['冷链件数'] >= 50 and row['冷链件数'] < 10 and row['实付金额'] >= 8000:
-            return '送货费40元'
-        elif row['大件件数'] + row['冷链件数'] < 50 and row['实付金额'] < 8000:
-            pay = (row['大件件数'] + row['冷链件数']) * 7
-            return f'送货费80元\n运费（{pay}）元'
-        elif row['大件件数'] + row['冷链件数'] < 50 and row['实付金额'] >= 8000:
-            return '送货费80元'
+    def style_modify(self, row):
+        if not isinstance(row, float):
+            print(row)
+            print(type(row))
+            return 'background-color: yellow'
         else:
-            return row['备注']
+            return ''
 
+    # 按照配送件数计算金额，写入备注列
+    def money_modify(self, row):
+        if row['冷链件数'] == 0:
+            if row['大件件数'] > 50:
+                return '送货费80'
+            else:
+                return row['备注']
+        else:
+            if row['大件件数'] + row['冷链件数'] >= 50 and row['冷链件数'] < 10 and row['实付金额'] < 8000:
+                pay = (row['大件件数'] + row['冷链件数']) * 7
+                return f'送货费40元\n运费（{pay}）元'
+            elif row['大件件数'] + row['冷链件数'] >= 50 and row['冷链件数'] < 10 and row['实付金额'] >= 8000:
+                return '送货费40元'
+            elif row['大件件数'] + row['冷链件数'] < 50 and row['实付金额'] < 8000:
+                pay = (row['大件件数'] + row['冷链件数']) * 7
+                return f'送货费80元\n运费（{pay}）元'
+            elif row['大件件数'] + row['冷链件数'] < 50 and row['实付金额'] >= 8000:
+                return '送货费80元'
+            else:
+                return row['备注']
+
+    # 写入西藏的数据，并将四川城市创建为新的空sheet
     def xizang_df(self):
         # 日期
         today = self.today_str
         df = self.df
         # 筛选符合西藏条件的数据
         xizang_df = df.loc[df['省份'] == '西藏自治区']
-        # 保存更改后的数据回Excel文件
-        xizang_df.to_excel(f'./{today}/{today}专线.xlsx', index=False, sheet_name='西藏')
+        # 新增几个空的表，以四川城市命名
+        sichuan_df = df.loc[df['省份'] != '西藏自治区']
+        # 按四川城市分类
+        groups = sichuan_df.groupby('城市')
+        categories = list(groups.groups.keys())
+        # 准备空表单
+        null_df = pd.DataFrame({'': ['']})
+        with pd.ExcelWriter(f'./{today}/{today}专线.xlsx') as writer:
+            # 添加设备背景色
+            xizang_df = xizang_df.style.applymap(self.style_modify, subset='设备')
+            xizang_df.to_excel(writer, index=False, sheet_name='西藏自治区')
+            for i in categories:
+                # 按城市名添加新的空表
+                null_df.to_excel(writer, sheet_name=i, index=False)
 
+    # 将四川的城市数据分类写入
     def sichuan_df(self):
         # 日期
         today = self.today_str
         df = self.df
         # 筛选符合四川条件的数据
         sichuan_df = df.loc[df['省份'] != '西藏自治区']
-        print(sichuan_df)
         # 按城市分类
         groups = sichuan_df.groupby('城市')
         categories = list(groups.groups.keys())
-        # for i in categories:
-        #     sichuan_city_df = sichuan_df.loc[df['城市'] == i]
-        #     # 保存更改后的数据回Excel文件
-        #     sichuan_city_df.to_excel(f'./{today}/{today}直送.xlsx', index=False, sheet_name=i)
-
         with pd.ExcelWriter(f'./{today}/{today}直送.xlsx') as writer:
             for i in categories:
                 sichuan_city_df = sichuan_df.loc[df['城市'] == i]
+                # 添加背景色
+                sichuan_city_df = sichuan_city_df.style.applymap(self.style_modify, subset='设备')
                 # 将 df1 写入一个名为 Sheet1 的 sheet 中
                 sichuan_city_df.to_excel(writer, index=False, sheet_name=i)
 
@@ -69,9 +96,11 @@ class KeNuo:
         os.makedirs(folder_path, exist_ok=True)
 
     def data_writer(self):
+        # 初处理过的df
         df = self.df
         # 按照货物计算价格后，写入备注
         df['备注'] = df.apply(self.money_modify, axis=1)
+        # 2次处理的df赋值
         self.df = df
         self.create_folder()
         self.xizang_df()
--- test.py
+++ test.py
@@ -27,13 +27,57 @@
 # df = pd.DataFrame(data)
 # # 将DataFrame写入Excel文件
 # df.to_excel('example.xlsx', index=False)
+# import pandas as pd
+# # 创建两个示例 DataFrame
+# df1 = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+# df2 = pd.DataFrame({'X': [7, 8, 9], 'Y': [10, 11, 12]})
+# # 创建一个 ExcelWriter 对象，指定文件名
+# with pd.ExcelWriter('example.xlsx') as writer:
+#     # 将 df1 写入一个名为 Sheet1 的 sheet 中
+#     df1.to_excel(writer, sheet_name='Sheet1')
+#     # 将 df2 写入一个名为 Sheet2 的 sheet 中
+#     df2.to_excel(writer, sheet_name='Sheet2')
+
+# class ZhiPi:
+#     def __init__(self, file_path):
+#         # 获取当前日期
+#         today = datetime.date.today()
+#         # 将日期格式化为字符串
+#         self.today_str = today.strftime('%Y-%m-%d')
+#         # 读取Excel文件
+#         self.df = pd.read_excel(file_path, sheet_name=0)
+#         # 删除第1列和第3列
+#         self.df = self.df.drop(self.df.columns[[0, 2]], axis=1)
+#         print(self.df)
+#
+#
+# ZhiPi(file_path=r'C:\Users\HeWenjun\Desktop\3.13可诺(1).xlsx')
+# num1 = int(input('input1:'))
+# num2 = int(input('input2:'))
+# if num1 < 10 and num2 < 10:
+#     print('small')
+# elif num1 < 10 < num2:
+#     print('medium')
+# elif num1 >= 10 and num2 >= 10:
+#     print('big')
 import pandas as pd
-# 创建两个示例 DataFrame
-df1 = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-df2 = pd.DataFrame({'X': [7, 8, 9], 'Y': [10, 11, 12]})
-# 创建一个 ExcelWriter 对象，指定文件名
-with pd.ExcelWriter('example.xlsx') as writer:
-    # 将 df1 写入一个名为 Sheet1 的 sheet 中
-    df1.to_excel(writer, sheet_name='Sheet1')
-    # 将 df2 写入一个名为 Sheet2 的 sheet 中
-    df2.to_excel(writer, sheet_name='Sheet2')
+import numpy as np
+import random
+
+# 创建一个包含随机数据的DataFrame
+df1 = pd.DataFrame(np.random.randint(0, 100, size=(10, 4)), columns=list('ABCD'))
+# 筛选出df1中B列的值大于50的行，并将结果保存到df2中
+df2 = df1[df1['B'] > 50]
+
+
+# 创建一个函数，用于生成样式
+def highlight(value):
+    if value > 50:
+        return 'background-color: yellow'
+    else:
+        return ''
+
+
+# 使用style.applymap()方法将样式应用到df2中
+styled_df = df2.style.applymap(highlight)
+styled_df.to_excel('style.xlsx')
